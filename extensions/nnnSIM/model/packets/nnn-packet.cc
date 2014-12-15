cf06f0e7fb8a0aedf31b921f375e66ab8ccfacd1
Jairo Eduardo Lopez <jelfn@sislacom.com>
2014-12-15 13:41:37 +0900

    Headers for nnn-packet.{h,cc}

--- extensions/nnnSIM/model/packets/nnn-packet.cc
+++ extensions/nnnSIM/model/packets/nnn-packet.cc
@@ -1,8 +1,20 @@
+/* -*- Mode: C++; c-file-style: "gnu" -*- */
 /*
- * nnn-packet.cc
+ * Copyright 2014 Waseda University, Sato Laboratory
+ *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
  *
- *  Created on: Dec 15, 2014
- *      Author: jelfn
+ *  nnn-packet.cc is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nnn-packet.cc is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with nnn-packet.cc.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "nnn-packet.h"
