977bcd13b8344268317e7b6a052ca8dd0af370cd
Jairo Lopez <jairo@ruri.waseda.jp>
2014-12-18 04:38:42 +0900

    Reimplementation of SO Wire Class

--- extensions/nnnSIM/model/wire/nnnsim/so/nnnsim-so.h
+++ extensions/nnnSIM/model/wire/nnnsim/so/nnnsim-so.h
@@ -16,7 +16,7 @@
  *  You should have received a copy of the GNU Affero Public License
  *  along with nnnsim-so.h.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include "../nnnsim-common-hdr.h"
 #include "../nnnsim-common.h"
 NNN_NAMESPACE_BEGIN
 /**
@@ -32,14 +32,11 @@ namespace wire {
     /**
      * @brief Routines to serialize/deserialize SO packets in nnnSIM format
      */
-    class SO : public Header
+    class SO : public CommonHeader<nnn::SO>
     {
     public:
       SO ();
       SO (Ptr<nnn::SO> so);
-
-      Ptr<nnn::SO>
-      GetSO ();
 
       static Ptr<Packet>
       ToWire (Ptr<const nnn::SO> so);
@@ -49,16 +46,12 @@ namespace wire {
 
       // from Header
       static TypeId GetTypeId (void);
-      virtual TypeId GetInstanceTypeId (void) const;
-      virtual void Print (std::ostream &os) const;
-      virtual uint32_t GetSerializedSize (void) const;
-      virtual void Serialize (Buffer::Iterator start) const;
-      virtual uint32_t Deserialize (Buffer::Iterator start);
+      TypeId GetInstanceTypeId (void) const;
+      uint32_t GetSerializedSize (void) const;
+      void Serialize (Buffer::Iterator start) const;
+      uint32_t Deserialize (Buffer::Iterator start);
 
-    private:
-      Ptr<nnn::SO> m_so_p;
     };
-
   }
 }
 
