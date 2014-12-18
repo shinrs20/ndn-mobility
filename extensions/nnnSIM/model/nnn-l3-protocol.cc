fe776c19b483e3c798a0354c326727ddae133326
Jairo Eduardo Lopez <lopez.jairo@miraclelinux.com>
2014-12-18 10:09:52 +0900

    NNN L3 Protocol indentation

--- extensions/nnnSIM/model/nnn-l3-protocol.cc
+++ extensions/nnnSIM/model/nnn-l3-protocol.cc
@@ -42,182 +42,182 @@
 NS_LOG_COMPONENT_DEFINE ("nnn.L3Protocol");
 
 namespace ns3 {
-namespace nnn {
+  namespace nnn {
 
-const uint16_t L3Protocol::ETHERNET_FRAME_TYPE = 0x7786;
+    const uint16_t L3Protocol::ETHERNET_FRAME_TYPE = 0x7786;
 
-NS_OBJECT_ENSURE_REGISTERED (L3Protocol);
+    NS_OBJECT_ENSURE_REGISTERED (L3Protocol);
 
-TypeId
-L3Protocol::GetTypeId (void)
-{
-	static TypeId tid = TypeId ("ns3::nnn::L3Protocol")
-    		.SetGroupName ("nnn")
-    		.SetParent<Object> ()
-    		.AddConstructor<L3Protocol> ()
-    		.AddAttribute ("FaceList", "List of faces associated with nnn stack",
-    				ObjectVectorValue (),
-    				MakeObjectVectorAccessor (&L3Protocol::m_faces),
-    				MakeObjectVectorChecker<Face> ())
-    				;
-	return tid;
-}
+    TypeId
+    L3Protocol::GetTypeId (void)
+    {
+      static TypeId tid = TypeId ("ns3::nnn::L3Protocol")
+    		    .SetGroupName ("nnn")
+    		    .SetParent<Object> ()
+    		    .AddConstructor<L3Protocol> ()
+    		    .AddAttribute ("FaceList", "List of faces associated with nnn stack",
+    		                   ObjectVectorValue (),
+    		                   MakeObjectVectorAccessor (&L3Protocol::m_faces),
+    		                   MakeObjectVectorChecker<Face> ())
+    		                   ;
+      return tid;
+    }
 
-L3Protocol::L3Protocol ()
-: m_faceCounter (0)
-{
-	NS_LOG_FUNCTION (this);
-}
+    L3Protocol::L3Protocol ()
+    : m_faceCounter (0)
+    {
+      NS_LOG_FUNCTION (this);
+    }
 
-L3Protocol::~L3Protocol ()
-{
-	NS_LOG_FUNCTION (this);
-}
+    L3Protocol::~L3Protocol ()
+    {
+      NS_LOG_FUNCTION (this);
+    }
 
-/*
- * This method is called by AddAgregate and completes the aggregation
- * by setting the node in the ndn stack
- */
-void
-L3Protocol::NotifyNewAggregate ()
-{
-	// not really efficient, but this will work only once
-	if (m_node == 0)
+    /*
+     * This method is called by AddAgregate and completes the aggregation
+     * by setting the node in the ndn stack
+     */
+    void
+    L3Protocol::NotifyNewAggregate ()
+    {
+      // not really efficient, but this will work only once
+      if (m_node == 0)
 	{
-		m_node = GetObject<Node> ();
-		if (m_node != 0)
-		{
-			NS_ASSERT_MSG (m_forwardingStrategy != 0,
-					"Forwarding strategy should be aggregated before NNNL3Protocol");
-		}
+	  m_node = GetObject<Node> ();
+	  if (m_node != 0)
+	    {
+	      NS_ASSERT_MSG (m_forwardingStrategy != 0,
+	                     "Forwarding strategy should be aggregated before NNNL3Protocol");
+	    }
 	}
-	if (m_forwardingStrategy == 0)
+      if (m_forwardingStrategy == 0)
 	{
-		m_forwardingStrategy = GetObject<ForwardingStrategy> ();
+	  m_forwardingStrategy = GetObject<ForwardingStrategy> ();
 	}
 
-	Object::NotifyNewAggregate ();
-}
+      Object::NotifyNewAggregate ();
+    }
 
-void
-L3Protocol::DoDispose (void)
-{
-	NS_LOG_FUNCTION (this);
+    void
+    L3Protocol::DoDispose (void)
+    {
+      NS_LOG_FUNCTION (this);
 
-	// for (FaceList::iterator i = m_faces.begin (); i != m_faces.end (); ++i)
-	//   {
-	//     *i = 0;
-	//   }
-	m_faces.clear ();
-	m_node = 0;
+      // for (FaceList::iterator i = m_faces.begin (); i != m_faces.end (); ++i)
+      //   {
+      //     *i = 0;
+      //   }
+      m_faces.clear ();
+      m_node = 0;
 
-	// Force delete on objects
-	m_forwardingStrategy = 0; // there is a reference to PIT stored in here
+      // Force delete on objects
+      m_forwardingStrategy = 0; // there is a reference to PIT stored in here
 
-	Object::DoDispose ();
-}
+      Object::DoDispose ();
+    }
 
-uint32_t
-L3Protocol::AddFace (const Ptr<Face> &face)
-{
-	NS_LOG_FUNCTION (this << &face);
+    uint32_t
+    L3Protocol::AddFace (const Ptr<Face> &face)
+    {
+      NS_LOG_FUNCTION (this << &face);
 
-	face->SetId (m_faceCounter); // sets a unique ID of the face. This ID serves only informational purposes
+      face->SetId (m_faceCounter); // sets a unique ID of the face. This ID serves only informational purposes
 
-	// ask face to register in lower-layer stack
-	face->RegisterNNNProtocolHandlers (
-			MakeCallback (&ForwardingStrategy::OnNULLp, m_forwardingStrategy),
-			MakeCallback (&ForwardingStrategy::OnSO, m_forwardingStrategy),
-			MakeCallback (&ForwardingStrategy::OnDO, m_forwardingStrategy),
-			MakeCallback (&ForwardingStrategy::OnEN, m_forwardingStrategy),
-			MakeCallback (&ForwardingStrategy::OnAEN, m_forwardingStrategy),
-			MakeCallback (&ForwardingStrategy::OnREN, m_forwardingStrategy),
-			MakeCallback (&ForwardingStrategy::OnDEN, m_forwardingStrategy),
-			MakeCallback (&ForwardingStrategy::OnINF, m_forwardingStrategy)
-			);
+      // ask face to register in lower-layer stack
+      face->RegisterNNNProtocolHandlers (
+	  MakeCallback (&ForwardingStrategy::OnNULLp, m_forwardingStrategy),
+	  MakeCallback (&ForwardingStrategy::OnSO, m_forwardingStrategy),
+	  MakeCallback (&ForwardingStrategy::OnDO, m_forwardingStrategy),
+	  MakeCallback (&ForwardingStrategy::OnEN, m_forwardingStrategy),
+	  MakeCallback (&ForwardingStrategy::OnAEN, m_forwardingStrategy),
+	  MakeCallback (&ForwardingStrategy::OnREN, m_forwardingStrategy),
+	  MakeCallback (&ForwardingStrategy::OnDEN, m_forwardingStrategy),
+	  MakeCallback (&ForwardingStrategy::OnINF, m_forwardingStrategy)
+      );
 
-	m_faces.push_back (face);
-	m_faceCounter++;
+      m_faces.push_back (face);
+      m_faceCounter++;
 
-	m_forwardingStrategy->AddFace (face); // notify that face is added
-	return face->GetId ();
-}
+      m_forwardingStrategy->AddFace (face); // notify that face is added
+      return face->GetId ();
+    }
 
-void
-L3Protocol::RemoveFace (Ptr<Face> face)
-{
-//	NS_LOG_FUNCTION (this << boost::cref (*face));
-//	// ask face to register in lower-layer stack
-//	face->UnRegisterProtocolHandlers ();
-//	Ptr<Pit> pit = GetObject<Pit> ();
-//
-//	// just to be on a safe side. Do the process in two steps
-//	std::list< Ptr<pit::Entry> > entriesToRemoves;
-//	for (Ptr<pit::Entry> pitEntry = pit->Begin (); pitEntry != 0; pitEntry = pit->Next (pitEntry))
-//	{
-//		pitEntry->RemoveAllReferencesToFace (face);
-//
-//		// If this face is the only for the associated FIB entry, then FIB entry will be removed soon.
-//		// Thus, we have to remove the whole PIT entry
-//		if (pitEntry->GetFibEntry ()->m_faces.size () == 1 &&
-//				pitEntry->GetFibEntry ()->m_faces.begin ()->GetFace () == face)
-//		{
-//			entriesToRemoves.push_back (pitEntry);
-//		}
-//	}
-//	BOOST_FOREACH (Ptr<pit::Entry> removedEntry, entriesToRemoves)
-//	{
-//		pit->MarkErased (removedEntry);
-//	}
-//
-//	FaceList::iterator face_it = find (m_faces.begin(), m_faces.end(), face);
-//	if (face_it == m_faces.end ())
-//	{
-//		return;
-//	}
-//	m_faces.erase (face_it);
-//
-//	GetObject<Fib> ()->RemoveFromAll (face);
-//	m_forwardingStrategy->RemoveFace (face); // notify that face is removed
-}
+    void
+    L3Protocol::RemoveFace (Ptr<Face> face)
+    {
+      //	NS_LOG_FUNCTION (this << boost::cref (*face));
+      //	// ask face to register in lower-layer stack
+      //	face->UnRegisterProtocolHandlers ();
+      //	Ptr<Pit> pit = GetObject<Pit> ();
+      //
+      //	// just to be on a safe side. Do the process in two steps
+      //	std::list< Ptr<pit::Entry> > entriesToRemoves;
+      //	for (Ptr<pit::Entry> pitEntry = pit->Begin (); pitEntry != 0; pitEntry = pit->Next (pitEntry))
+      //	{
+      //		pitEntry->RemoveAllReferencesToFace (face);
+      //
+      //		// If this face is the only for the associated FIB entry, then FIB entry will be removed soon.
+      //		// Thus, we have to remove the whole PIT entry
+      //		if (pitEntry->GetFibEntry ()->m_faces.size () == 1 &&
+      //				pitEntry->GetFibEntry ()->m_faces.begin ()->GetFace () == face)
+      //		{
+      //			entriesToRemoves.push_back (pitEntry);
+      //		}
+      //	}
+      //	BOOST_FOREACH (Ptr<pit::Entry> removedEntry, entriesToRemoves)
+      //	{
+      //		pit->MarkErased (removedEntry);
+      //	}
+      //
+      //	FaceList::iterator face_it = find (m_faces.begin(), m_faces.end(), face);
+      //	if (face_it == m_faces.end ())
+      //	{
+      //		return;
+      //	}
+      //	m_faces.erase (face_it);
+      //
+      //	GetObject<Fib> ()->RemoveFromAll (face);
+      //	m_forwardingStrategy->RemoveFace (face); // notify that face is removed
+    }
 
-Ptr<Face>
-L3Protocol::GetFace (uint32_t index) const
-{
-	NS_ASSERT (0 <= index && index < m_faces.size ());
-	return m_faces[index];
-}
+    Ptr<Face>
+    L3Protocol::GetFace (uint32_t index) const
+    {
+      NS_ASSERT (0 <= index && index < m_faces.size ());
+      return m_faces[index];
+    }
 
-Ptr<Face>
-L3Protocol::GetFaceById (uint32_t index) const
-{
-	BOOST_FOREACH (const Ptr<Face> &face, m_faces) // this function is not supposed to be called often, so linear search is fine
-    		{
-		if (face->GetId () == index)
-			return face;
-    		}
-	return 0;
-}
+    Ptr<Face>
+    L3Protocol::GetFaceById (uint32_t index) const
+    {
+      BOOST_FOREACH (const Ptr<Face> &face, m_faces) // this function is not supposed to be called often, so linear search is fine
+    		    {
+	if (face->GetId () == index)
+	  return face;
+    		    }
+      return 0;
+    }
 
-Ptr<Face>
-L3Protocol::GetFaceByNetDevice (Ptr<NetDevice> netDevice) const
-{
-	BOOST_FOREACH (const Ptr<Face> &face, m_faces) // this function is not supposed to be called often, so linear search is fine
-    		{
-		Ptr<NetDeviceFace> netDeviceFace = DynamicCast<NetDeviceFace> (face);
-		if (netDeviceFace == 0) continue;
+    Ptr<Face>
+    L3Protocol::GetFaceByNetDevice (Ptr<NetDevice> netDevice) const
+    {
+      BOOST_FOREACH (const Ptr<Face> &face, m_faces) // this function is not supposed to be called often, so linear search is fine
+    		    {
+	Ptr<NetDeviceFace> netDeviceFace = DynamicCast<NetDeviceFace> (face);
+	if (netDeviceFace == 0) continue;
 
-		if (netDeviceFace->GetNetDevice () == netDevice)
-			return face;
-    		}
-	return 0;
-}
+	if (netDeviceFace->GetNetDevice () == netDevice)
+	  return face;
+    		    }
+      return 0;
+    }
 
-uint32_t
-L3Protocol::GetNFaces (void) const
-{
-	return m_faces.size ();
-}
+    uint32_t
+    L3Protocol::GetNFaces (void) const
+    {
+      return m_faces.size ();
+    }
 
 } //namespace ndn
 } //namespace ns3
--- extensions/nnnSIM/model/nnn-l3-protocol.h
+++ extensions/nnnSIM/model/nnn-l3-protocol.h
@@ -30,131 +30,131 @@
 
 namespace ns3 {
 
-class Packet;
-class NetDevice;
-class Node;
-class Header;
+  class Packet;
+  class NetDevice;
+  class Node;
+  class Header;
 
-namespace nnn {
+  namespace nnn {
 
-class Face;
-class ForwardingStrategy;
+    class Face;
+    class ForwardingStrategy;
 
-/**
- * \defgroup nnn nnnSIM: nnn simulation module
- *
- * This is a modular implementation of NNN protocol for NS-3
- */
-/**
- * \ingroup nnn
- * \brief Implementation network-layer of NNN stack
- *
- * This class defines the API to manipulate the following aspects of
- * the NNN stack implementation:
- * -# register a face (Face-derived object) for use by the NNN
- *    layer
- * 
- * Each Face-derived object has conceptually a single NNN
- * interface associated with it.
- *
- * In addition, this class defines NNN packet coding constants
- *
- * \see Face, ForwardingStrategy
- */
-class L3Protocol :
-		public Object
-{
-public:
-	typedef std::vector<Ptr<Face> > FaceList;
+    /**
+     * \defgroup nnn nnnSIM: nnn simulation module
+     *
+     * This is a modular implementation of NNN protocol for NS-3
+     */
+    /**
+     * \ingroup nnn
+     * \brief Implementation network-layer of NNN stack
+     *
+     * This class defines the API to manipulate the following aspects of
+     * the NNN stack implementation:
+     * -# register a face (Face-derived object) for use by the NNN
+     *    layer
+     *
+     * Each Face-derived object has conceptually a single NNN
+     * interface associated with it.
+     *
+     * In addition, this class defines NNN packet coding constants
+     *
+     * \see Face, ForwardingStrategy
+     */
+    class L3Protocol :
+	public Object
+    {
+    public:
+      typedef std::vector<Ptr<Face> > FaceList;
 
-	/**
-	 * \brief Interface ID
-	 *
-	 * \return interface ID
-	 */
-	static TypeId GetTypeId ();
+      /**
+       * \brief Interface ID
+       *
+       * \return interface ID
+       */
+      static TypeId GetTypeId ();
 
-	static const uint16_t ETHERNET_FRAME_TYPE; ///< @brief Ethernet Frame Type of Nnn
-	//static const uint16_t IP_STACK_PORT;       ///< @brief TCP/UDP port for NNN stack
-	// static const uint16_t IP_PROTOCOL_TYPE;    ///< \brief IP protocol type of NNN
+      static const uint16_t ETHERNET_FRAME_TYPE; ///< @brief Ethernet Frame Type of Nnn
+      //static const uint16_t IP_STACK_PORT;       ///< @brief TCP/UDP port for NNN stack
+      // static const uint16_t IP_PROTOCOL_TYPE;    ///< \brief IP protocol type of NNN
 
-	/**
-	 * \brief Default constructor. Creates an empty stack without forwarding strategy set
-	 */
-	L3Protocol ();
-	virtual ~L3Protocol  ();
+      /**
+       * \brief Default constructor. Creates an empty stack without forwarding strategy set
+       */
+      L3Protocol ();
+      virtual ~L3Protocol  ();
 
-	/**
-	 * \brief Add face to Nnn stack
-	 *
-	 * \param face smart pointer to NnnFace-derived object
-	 * (NnnLocalFace, NnnNetDeviceFace, NnnUdpFace) \returns the
-	 * index of the Nnn interface added.
-	 *
-	 * \see NnnLocalFace, NnnNetDeviceFace, NnnUdpFace
-	 */
-	virtual uint32_t
-	AddFace (const Ptr<Face> &face);
+      /**
+       * \brief Add face to Nnn stack
+       *
+       * \param face smart pointer to NnnFace-derived object
+       * (NnnLocalFace, NnnNetDeviceFace, NnnUdpFace) \returns the
+       * index of the Nnn interface added.
+       *
+       * \see NnnLocalFace, NnnNetDeviceFace, NnnUdpFace
+       */
+      virtual uint32_t
+      AddFace (const Ptr<Face> &face);
 
-	/**
-	 * \brief Get current number of faces added to Nnn stack
-	 *
-	 * \returns the number of faces
-	 */
-	virtual uint32_t
-	GetNFaces () const;
+      /**
+       * \brief Get current number of faces added to Nnn stack
+       *
+       * \returns the number of faces
+       */
+      virtual uint32_t
+      GetNFaces () const;
 
-	/**
-	 * \brief Get face by face index
-	 * \param face The face number (number in face list)
-	 * \returns The NnnFace associated with the Nnn face number.
-	 */
-	virtual Ptr<Face>
-	GetFace (uint32_t face) const;
+      /**
+       * \brief Get face by face index
+       * \param face The face number (number in face list)
+       * \returns The NnnFace associated with the Nnn face number.
+       */
+      virtual Ptr<Face>
+      GetFace (uint32_t face) const;
 
-	/**
-	 * \brief Get face by face ID
-	 * \param face The face ID number
-	 * \returns The NnnFace associated with the Nnn face number.
-	 */
-	virtual Ptr<Face>
-	GetFaceById (uint32_t face) const;
+      /**
+       * \brief Get face by face ID
+       * \param face The face ID number
+       * \returns The NnnFace associated with the Nnn face number.
+       */
+      virtual Ptr<Face>
+      GetFaceById (uint32_t face) const;
 
-	/**
-	 * \brief Remove face from Nnn stack (remove callbacks)
-	 */
-	virtual void
-	RemoveFace (Ptr<Face> face);
+      /**
+       * \brief Remove face from Nnn stack (remove callbacks)
+       */
+      virtual void
+      RemoveFace (Ptr<Face> face);
 
-	/**
-	 * \brief Get face for NetDevice
-	 */
-	virtual Ptr<Face>
-	GetFaceByNetDevice (Ptr<NetDevice> netDevice) const;
+      /**
+       * \brief Get face for NetDevice
+       */
+      virtual Ptr<Face>
+      GetFaceByNetDevice (Ptr<NetDevice> netDevice) const;
 
-protected:
-	virtual void DoDispose (void); ///< @brief Do cleanup
+    protected:
+      virtual void DoDispose (void); ///< @brief Do cleanup
 
-	/**
-	 * This function will notify other components connected to the node that a new stack member is now connected
-	 * This will be used to notify Layer 3 protocol of layer 4 protocol stack to connect them together.
-	 */
-	virtual void NotifyNewAggregate ();
+      /**
+       * This function will notify other components connected to the node that a new stack member is now connected
+       * This will be used to notify Layer 3 protocol of layer 4 protocol stack to connect them together.
+       */
+      virtual void NotifyNewAggregate ();
 
-private:
-	L3Protocol (const L3Protocol  &); ///< copy constructor is disabled
-	L3Protocol  &operator = (const L3Protocol  &); ///< copy operator is disabled
+    private:
+      L3Protocol (const L3Protocol  &); ///< copy constructor is disabled
+      L3Protocol  &operator = (const L3Protocol  &); ///< copy operator is disabled
 
-private:
-	uint32_t m_faceCounter; ///< \brief counter of faces. Increased every time a new face is added to the stack
-	FaceList m_faces; ///< \brief list of faces that belongs to nnn stack on this node
+    private:
+      uint32_t m_faceCounter; ///< \brief counter of faces. Increased every time a new face is added to the stack
+      FaceList m_faces; ///< \brief list of faces that belongs to nnn stack on this node
 
-	// These objects are aggregated, but for optimization, get them here
-	Ptr<Node> m_node; ///< \brief node on which nnn stack is installed
-	Ptr<ForwardingStrategy> m_forwardingStrategy; ///< \brief smart pointer to the selected forwarding strategy
-};
+      // These objects are aggregated, but for optimization, get them here
+      Ptr<Node> m_node; ///< \brief node on which nnn stack is installed
+      Ptr<ForwardingStrategy> m_forwardingStrategy; ///< \brief smart pointer to the selected forwarding strategy
+    };
 
-} // namespace nnn
+  } // namespace nnn
 } // namespace ns3
 
 #endif /* NNN_L3_PROTOCOL_H */
