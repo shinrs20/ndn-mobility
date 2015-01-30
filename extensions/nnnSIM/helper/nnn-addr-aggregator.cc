46c46bfc8afb4eead8de10779d3ff690858694ff
Jairo Lopez <jairo@ruri.waseda.jp>
2015-01-31 03:17:52 +0900

    NNN 3N name aggregator structure.
    Copied from MDO, which apparently is not required.
    
    xD

---
+++ extensions/nnnSIM/helper/nnn-addr-aggregator.cc
@@ -0,0 +1,222 @@
+/* -*- Mode: C++; c-file-style: "gnu" -*- */
+/*
+ * Copyright 2015 Waseda University, Sato Laboratory
+ *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
+ *
+ *  nnn-addr-aggregator.cc is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nnn-addr-aggregator.cc is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with nnn-addr-aggregator.cc.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <ns3-dev/ns3/log.h>
+
+#include "nnn-addr-aggregator.h"
+
+NS_LOG_COMPONENT_DEFINE("nnn.AddrAggregator");
+
+namespace ns3
+{
+  namespace nnn
+  {
+
+    NNNAddrAggregator::NNNAddrAggregator ()
+    : m_totaladdr (0)
+    , m_totaldest (0)
+    {
+    }
+
+    NNNAddrAggregator::~NNNAddrAggregator ()
+    {
+    }
+
+    uint16_t
+    NNNAddrAggregator::GetNumDestinations(Ptr<NNNAddress> sector)
+    {
+      return m_sectorNum[*sector];
+    }
+
+    uint16_t
+    NNNAddrAggregator::GetNumDistinctDestinations () const
+    {
+      return super::getPolicy().size();
+    }
+
+    uint16_t
+    NNNAddrAggregator::GetNumTotalDestinations () const
+    {
+      uint16_t dests = 0;
+
+      Ptr<const NNNAddrEntry> tmp;
+
+      for (tmp = Begin(); tmp != End(); tmp = Next(tmp))
+	{
+	  dests += tmp->GetNumAddresses();
+	}
+
+      return dests;
+    }
+
+    std::vector<Ptr<NNNAddress> >
+    NNNAddrAggregator::GetDestinations (Ptr<NNNAddress> sector)
+    {
+      super::iterator item = super::find_exact(*sector);
+
+      if (item == super::end ())
+	return std::vector<Ptr<NNNAddress> > ();
+      else
+	{
+	  if (item->payload() == 0)
+	    return std::vector<Ptr<NNNAddress> > ();
+	  else
+	    return item->payload()->GetAddresses();
+	}
+    }
+
+    std::vector<Ptr<NNNAddress> >
+    NNNAddrAggregator::GetCompleteDestinations (Ptr<NNNAddress> sector)
+    {
+      super::iterator item = super::find_exact(*sector);
+
+      if (item == super::end ())
+	return std::vector<Ptr<NNNAddress> > ();
+      else
+	{
+	  if (item->payload() == 0)
+	    return std::vector<Ptr<NNNAddress> > ();
+	  else
+	    return item->payload()->GetCompleteAddresses();
+	}
+    }
+
+    std::vector<Ptr<NNNAddress> >
+    NNNAddrAggregator::GetDistinctDestinations() const
+    {
+      std::vector<Ptr<NNNAddress> > distinct;
+      Ptr<const NNNAddrEntry> tmp;
+
+      for (tmp = Begin (); tmp != End (); tmp = Next(tmp))
+	{
+	  distinct.push_back(tmp->GetSector());
+	}
+
+      return distinct;
+    }
+
+    std::vector<Ptr<NNNAddress> >
+    NNNAddrAggregator::GetTotalDestinations () const
+    {
+      std::vector<Ptr<NNNAddress> > distinct;
+      Ptr<const NNNAddrEntry> tmp;
+
+      for (tmp = Begin (); tmp != End (); tmp = Next(tmp))
+	{
+	  std::vector<Ptr<NNNAddress> > tmpV = tmp->GetCompleteAddresses ();
+	  distinct.insert(distinct.end(), tmpV.begin(), tmpV.end());
+	}
+
+      return distinct;
+    }
+
+    void
+    NNNAddrAggregator::AddDestination(Ptr<NNNAddress> addr)
+    {
+      NS_LOG_FUNCTION(this << *addr);
+
+      NS_LOG_INFO("MDO: Inserting " << *addr);
+
+      Ptr<NNNAddress> sector = Create<NNNAddress> (addr->getSectorName());
+      Ptr<NNNAddress> lastlabel = Create<NNNAddress> (addr->getLastLabel());
+
+      std::pair< super::iterator, bool> result = super::insert(*sector, 0);
+
+      if (result.first != super::end ())
+	{
+	  if (result.second)
+	    {
+	      NS_LOG_INFO("MDO: New position sector: " << *sector << " lastlabel: " << *lastlabel);
+	      Ptr<NNNAddrEntry> newEntry = Create<NNNAddrEntry> ();
+
+	      newEntry->SetSector(sector);
+	      newEntry->AddAddress (lastlabel);
+	      newEntry->SetTrie (result.first);
+	      result.first->set_payload (newEntry);
+
+	      m_totaldest++;
+	      m_totaladdr++;
+	      m_sectorNum[*sector] = 1;
+	    }
+	  else
+	    {
+	      NS_LOG_INFO("MDO: Sector already present: " << *sector << " lastlabel: " << *lastlabel);
+	      Ptr<NNNAddress> apparent = result.first->payload()->GetSector();
+	      NS_LOG_INFO("Testing against sector: " << *apparent);
+
+	      if (*apparent == *sector)
+		{
+		  result.first->payload()->AddAddress(lastlabel);
+		  m_totaladdr++;
+		  m_sectorNum[*sector]++;
+		}
+	    }
+	}
+    }
+
+    void
+    NNNAddrAggregator::Print (std::ostream &os) const
+    {
+
+    }
+
+    Ptr<const NNNAddrEntry>
+    NNNAddrAggregator::Begin () const
+    {
+      super::parent_trie::const_recursive_iterator item (super::getTrie ());
+      super::parent_trie::const_recursive_iterator end (0);
+      for (; item != end; item++)
+	{
+	  if (item->payload () == 0) continue;
+	  break;
+	}
+
+      if (item == end)
+	return End ();
+      else
+	return item->payload ();
+    }
+
+    Ptr<const NNNAddrEntry>
+    NNNAddrAggregator::End () const
+    {
+      return 0;
+    }
+
+    Ptr<const NNNAddrEntry>
+    NNNAddrAggregator::Next (Ptr<const NNNAddrEntry> from) const
+    {
+      if (from == 0) return 0;
+
+      super::parent_trie::const_recursive_iterator item (*StaticCast<const NNNAddrEntry> (from)->to_iterator ());
+      super::parent_trie::const_recursive_iterator end (0);
+      for (item++; item != end; item++)
+	{
+	  if (item->payload () == 0) continue;
+	  break;
+	}
+
+      if (item == end)
+	return End ();
+      else
+	return item->payload ();
+    }
+  } /* namespace nnn */
+} /* namespace ns3 */
---
+++ extensions/nnnSIM/helper/nnn-addr-aggregator.h
@@ -0,0 +1,97 @@
+/* -*- Mode: C++; c-file-style: "gnu" -*- */
+/*
+ * Copyright 2015 Waseda University, Sato Laboratory
+ *   Author: Jairo Eduardo Lopez <jairo@ruri.waseda.jp>
+ *
+ *  nnn-addr-aggregator.h is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  nnn-addr-aggregator.h is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero Public License
+ *  along with nnn-addr-aggregator.h.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#ifndef NNN_ADDR_AGGREGATOR_H_
+#define NNN_ADDR_AGGREGATOR_H_
+
+#include "../model/packets/nnn-packet.h"
+#include "../model/packets/utils/nnn-addr-entry.h"
+#include "../model/naming/nnn-address.h"
+#include "../utils/trie/trie.h"
+#include "../utils/trie/counting-policy.h"
+#include "../utils/trie/trie-with-policy.h"
+
+namespace ns3
+{
+  namespace nnn
+  {
+
+    class NNNAddrAggregator : SimpleRefCount<NNNAddrAggregator>,
+    protected ns3::nnn::nnnSIM::trie_with_policy<
+    NNNAddress,
+    ns3::nnn::nnnSIM::smart_pointer_payload_traits<NNNAddrEntry>,
+    ns3::nnn::nnnSIM::counting_policy_traits
+    >
+    {
+    public:
+      typedef ns3::nnn::nnnSIM::trie_with_policy<
+	  NNNAddress,
+	  ns3::nnn::nnnSIM::smart_pointer_payload_traits<NNNAddrEntry>,
+	  ns3::nnn::nnnSIM::counting_policy_traits
+      > super;
+
+      NNNAddrAggregator ();
+      virtual
+      ~NNNAddrAggregator ();
+
+      uint16_t
+      GetNumDestinations (Ptr<NNNAddress> sector);
+
+      uint16_t
+      GetNumDistinctDestinations () const;
+
+      uint16_t
+      GetNumTotalDestinations () const;
+
+      std::vector<Ptr<NNNAddress> >
+      GetDestinations (Ptr<NNNAddress> sector);
+
+      std::vector<Ptr<NNNAddress> >
+      GetCompleteDestinations (Ptr<NNNAddress> sector);
+
+      std::vector<Ptr<NNNAddress> >
+      GetDistinctDestinations () const;
+
+      std::vector<Ptr<NNNAddress> >
+      GetTotalDestinations () const;
+
+      void
+      AddDestination (Ptr<NNNAddress> addr);
+
+      void
+      Print (std::ostream &os) const;
+
+      Ptr<const NNNAddrEntry>
+      Begin () const;
+
+      Ptr<const NNNAddrEntry>
+      End () const;
+
+      Ptr<const NNNAddrEntry>
+      Next (Ptr<const NNNAddrEntry> from) const;
+
+    private:
+      uint16_t m_totaladdr;
+      uint16_t m_totaldest;
+      std::map<NNNAddress,uint16_t> m_sectorNum;
+    };
+  } /* namespace nnn */
+} /* namespace ns3 */
+
+#endif /* NNN_ADDR_AGGREGATOR_H_ */
