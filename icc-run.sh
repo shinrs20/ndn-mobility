cac559c8eac69466c07c06b7dad75718bb833413
Jairo Eduardo Lopez <jelfn@sislacom.com>
2015-05-06 19:46:05 +0900

    Added 4.2, 7, 8.4 and 9.8 m/s to tests

--- icc-run.sh
+++ icc-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SPEED="1.40 2.80 5.60 11.20"
+SPEED="1.40 2.80 4.20 5.60 7.00 8.40 9.80 11.20"
 
 for i in $SPEED
 do
@@ -26,11 +26,11 @@ do
     # App Delay
     # Consumer Mobility
     echo "Creating Consumer mobility network delay $i"
-    ./graphs/app-data-compare.R -d -f results/MobilityICC-app-delays-smart-con-$FNAME-01-001-004.txt -c results/MobilityICC-app-delays-3n-con-$FNAME-01-001-004.txt --str1="NDN Smart Flooding" --str2="3N + Smart Flooding" -t "Mobile Consumer" -s 20
+    ./graphs/app-data-compare.R -r -d -f results/MobilityICC-app-delays-smart-con-$FNAME-01-001-004.txt -c results/MobilityICC-app-delays-3n-con-$FNAME-01-001-004.txt --str1="NDN Smart Flooding" --str2="3N + Smart Flooding" -t "Mobile Consumer" -s 20
 
     # Producer Mobility
     echo "Creating Producer mobility network delay $i"
-    ./graphs/app-data-compare.R -d -f results/MobilityICC-app-delays-smart-prod-$FNAME-01-001-004.txt -c results/MobilityICC-app-delays-3n-prod-$FNAME-01-001-004.txt --str1="NDN Smart Flooding" --str2="3N + Smart Flooding" -t "Mobile Producer" -s 20
+    ./graphs/app-data-compare.R -r -d -f results/MobilityICC-app-delays-smart-prod-$FNAME-01-001-004.txt -c results/MobilityICC-app-delays-3n-prod-$FNAME-01-001-004.txt --str1="NDN Smart Flooding" --str2="3N + Smart Flooding" -t "Mobile Producer" -s 20
 
     # Aggregate packets
     # Consumer Mobility
@@ -42,7 +42,7 @@ do
     ./graphs/int-tr-compare.R -e 7 -f results/MobilityICC-aggregate-trace-smart-prod-$FNAME-01-001-004.txt -c results/MobilityICC-aggregate-trace-3n-prod-$FNAME-01-001-004.txt -t "Consumer with Mobile Producer" -s 20 --str1="NDN Smart Flooding" --str2="3N + Smart Flooding"
 
     # Special Producer Mobility graph
-    ./graphs/app-data-j.R -s 20 -d -f results/MobilityICC-app-delays-3n-prod-$FNAME-01-001-004.txt -t "Application using 3N + Smart Flooding"
+    ./graphs/app-data-j.R -r -s 20 -d -f results/MobilityICC-app-delays-3n-prod-$FNAME-01-001-004.txt -t "Application using 3N + Smart Flooding"
 
     # Data rate
     # Consumer Mobility
